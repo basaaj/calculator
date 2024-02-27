818bbdbb7c9d3fac2a6007e064c5e35dea500d09
j <93398891+basaaj@users.noreply.github.com>
2024-02-27 12:41:16 -0600

    Implement operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const firstNum = 0;
+const secondNum = 0;
+const operator = "+";
+
 function add(a, b) {
     return a + b;
 }
@@ -12,5 +16,23 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(op, a, b) {
+    if (op === "+") {
+        multiply(a,b);
+    }
+
+    else if (op === "-") {
+        subtract(a,b);
+    }
+
+    else if (op === "*") {
+        multiply(a,b);
+    }
+
+    else {
+        divide(a,b);
+    }
 }
 
