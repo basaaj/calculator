5b9532b2a92786c1e9cb22a131fa70dfd8812aa1
j <93398891+basaaj@users.noreply.github.com>
2024-02-28 20:48:54 -0600

    Handle multiple operations

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const secondNum = 0;
 const operator = "+";
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.display');
+let items = [];
 
 function add(a, b) {
     return a + b;
@@ -42,25 +43,38 @@ buttons.forEach(button => {
     button.addEventListener('click', function() {
         if (button.value === "clear") {
             display.textContent = 0;
+            items.splice(0, items.length);
         }
 
         else if (button.value === "equals") {
-            let items = display.textContent.split(" ");
             let soln = operate(items[1], Number(items[0]), Number(items[2]));
             display.textContent = soln;
+            items.splice(0, items.length);
+            items.push(soln);
         }
 
         else if (button.value === "operator") {
+            items.push(button.textContent);
+
+            if (items.length >= 3) {
+                let soln = operate(items[1], Number(items[0]), Number(items[2]));
+                display.textContent = soln;
+                items.splice(0, items.length);
+                items.push(soln, button.textContent);
+                console.log("bonk");
+            }
+
             display.textContent += " " + button.textContent + " ";
         }
 
         else if (display.textContent == 0) {
+            items.push(button.textContent);
             display.textContent = button.textContent;
         }
 
         else {
+            items.push(button.textContent);
             display.textContent += button.textContent;
         }
     });
 });
-
